4d1647653a6abcc492cf23d7adb8bb8a4df019c3
zygisau <zygimantasaugunas@gmail.com>
2019-12-27 12:00:00 +0200

    UPD: Readme file.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Simple_Space_Shooter
 This is a simple space shooter created in javascript to show how powerfull canvas element is
+
+Whole project is on github:  https://github.com/zygisau/Simple_Space_Shooter 
+
---
+++ time-machine.sh
@@ -0,0 +1,7 @@
+export Y=2019
+export M=12
+export D=27
+export GIT_COMMITTER_DATE="$Y-$M-$D 15:34:07"
+export GIT_AUTHOR_DATE="$Y-$M-$D 15:34:07"
+git add *
+git commit --date="$Y-$M-$D 12:00:00" -m "UPD: Readme file."
